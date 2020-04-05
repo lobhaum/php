a20884aacedf00ee036abb4cbbeb575e28011886
lobhaum <contato@eduardoluciano.com.br>
2020-04-05 11:38:06 -0300

    acrescido a rota com verbo post para /series/create

--- alura/Laravel/controle-series/routes/web.php
+++ alura/Laravel/controle-series/routes/web.php
@@ -17,3 +17,4 @@ Route::get('/', function () {
 
 Route::get('/series', 'SeriesController@index');
 Route::get('/series/create', 'SeriesController@create');
+Route::post('/series/create', 'SeriesController@store');
