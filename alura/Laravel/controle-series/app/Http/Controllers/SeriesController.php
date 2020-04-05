68516e95b4750fa154fc5504411679f5502e0568
lobhaum <contato@eduardoluciano.com.br>
2020-04-05 11:40:00 -0300

    Tirado lista com series padrão, acrescentado metodo store usando metodo estatico create

--- alura/Laravel/controle-series/app/Http/Controllers/SeriesController.php
+++ alura/Laravel/controle-series/app/Http/Controllers/SeriesController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = [
+        $series = Serie::all();
+        /* $series = [
             'Grey\'s Anatomy',
             'Lost',
             'Agents of SHIELD',
@@ -17,7 +19,7 @@ class SeriesController extends Controller
             'How I met your mother',
             'Big Bang Theory',
             'The Simpsons',
-        ];
+        ]; */
 
         return view('series.index', compact('series'));
     }
@@ -25,5 +27,15 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nome = $request->nome;
+        $serie = Serie::create($request->all());
+        /* $serie = new Serie();
+        $serie->nome = $nome;
+         var_dump($serie->save());*/
+        echo "Série com id ({$serie->id}) criada: ({$serie->nome})";
     }
 }
