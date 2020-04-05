449efa124fa92879cb43b291c3260443b386acd4
lobhaum <contato@eduardoluciano.com.br>
2020-04-05 11:38:30 -0300

    refatorado

--- alura/Laravel/controle-series/resources/views/series/index.blade.php
+++ alura/Laravel/controle-series/resources/views/series/index.blade.php
@@ -8,8 +8,8 @@
     @section('conteudo')
         <a href="/series/create" class="btn btn-dark mb-2">Adicionar</a>
         <ul class="list-group">
-            <?php foreach ($series as $serie) { ?>
-            <li class="list-group-item"><?php echo $serie; ?></li>
-            <?php }?>
+            @foreach ($series as $serie)
+            <li class="list-group-item">{{ $serie->nome }}</li>
+            @endforeach
         </ul>
     @endsection
