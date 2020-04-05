b99b163f445916ef15599b38fa5b53a8d94f8e8e
lobhaum <contato@eduardoluciano.com.br>
2020-04-05 09:59:23 -0300

    refatorado create extendendo layout.blade

---
+++ alura/Laravel/controle-series/resources/views/series/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+    @section('titulo')
+        Adicionar Série
+    @endsection
+    @section('cabecalho')
+        Adicionar Série
+    @endsection
+    @section('conteudo')
+        <form action="" method="POST">
+            <div class="form-group">
+                <label for="nome" >Nome:</label>
+                <input type="text" class="form-control" name="nome" id="nome">
+            </div>
+            <button class="btn btn-primary">Adicionar</button>
+        </form>
+    @endsection
+
+
+
