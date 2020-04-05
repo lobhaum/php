aeb77f79eabb920f1f43dbe37778ae94dae132d7
lobhaum <contato@eduardoluciano.com.br>
2020-04-05 06:28:24 -0300

    criado lista e rota de series

--- alura/Laravel/controle-series/routes/web.php
+++ alura/Laravel/controle-series/routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/series', function () {
+    $series = [
+        'Grey\'s Anatomy',
+        'Lost',
+        'Agents of SHIELD',
+        'Brooklyn Nine Nine',
+        'Friends',
+        'How I met your mother',
+        'Big Bang Theory',
+        'The Simpsons',
+    ];
+    $html = '<ul>';
+    foreach ($series as $serie) {
+        $html .= "<li>{$serie}</li>";
+    }
+    $html .= '</ul>';
+    echo $html;
+});
