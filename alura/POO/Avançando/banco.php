99060009d77210401c42ff8a521dfc76dab6cf85
lobhaum <contato@eduardoluciano.com.br>
2020-04-06 07:35:19 -0300

    incluso autoload class

--- alura/POO/Avançando/banco.php
+++ alura/POO/Avançando/banco.php
@@ -1,9 +1,15 @@
 <?php
+
+require_once 'Config/AutoLoad.php'; // carrega arquivo autoload
+
+use Alura\Banco\Config\AutoLoad; // usa o pacote autoload
+
+AutoLoad::getNamespace(); // instanciando o namespace estaticamente
+
 use Alura\Banco\Modelo\Conta\Conta;
-use Alura\Banco\Modelo\Endereco;
 use Alura\Banco\Modelo\Conta\Titular;
 use Alura\Banco\Modelo\CPF;
-
+use Alura\Banco\Modelo\Endereco;
 
 $endereco = new Endereco('Petrópolis', 'um bairro', 'minha rua', '71B');
 $vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
@@ -11,9 +17,9 @@ $primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
 
-echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
-echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
-echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+echo $primeiraConta->recuperaNomeTitular().PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular().PHP_EOL;
+echo $primeiraConta->recuperaSaldo().PHP_EOL;
 
 $patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $endereco);
 $segundaConta = new Conta($patricia);
