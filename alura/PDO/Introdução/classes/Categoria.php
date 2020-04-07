b8bf6cc1b006cb619362095d2395100c1d5e4ffe
lobhaum <contato@eduardoluciano.com.br>
2020-04-07 13:09:44 -0300

    formatado script

--- alura/PDO/Introdução/classes/Categoria.php
+++ alura/PDO/Introdução/classes/Categoria.php
@@ -2,16 +2,15 @@
 
 class Categoria
 {
-
     public $id;
     public $nome;
 
-    public function listar()
+    public function listar(): array
     {
-        $query = "SELECT id, nome FROM categorias";
-        $conexao = new PDO('mysql:host=127.0.0.1;dbname=estoque', 'root', 'alura');
+        $query = 'SELECT id, nome FROM categorias';
+        $conexao = new PDO('mysql:host=192.168.1.100;dbname=estoque', 'programador', 'Brasil2020');
         $resultado = $conexao->query($query);
-        $lista = $resultado->fetchAll();
-        return $lista;
+
+        return $resultado->fetchAll();
     }
 }
