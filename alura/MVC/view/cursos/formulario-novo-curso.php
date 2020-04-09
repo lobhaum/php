d3048577ff7563338e821376dcf493be09ff568a
lobhaum <contato@eduardoluciano.com.br>
2020-04-09 13:47:45 -0300

    view

---
+++ alura/MVC/view/cursos/formulario-novo-curso.php
@@ -0,0 +1,9 @@
+<?php include __DIR__ . "/../header.html.php";?>
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="descricao">Descricao</label>
+            <input type="text" name="descricao" id="descricao" class="form-control">
+        </div>
+        <button class="btn btn-primary">Salvar</button>
+    </form>
+<?php include __DIR__ . "/../bottom.html.php";?>
