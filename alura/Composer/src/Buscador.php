6d5fe89788369932bed24b7af0a373179180cd83
lobhaum <contato@eduardoluciano.com.br>
2020-04-11 09:43:33 -0300

    Refatorado buscas-cursos.php para a classe Buscador.php

---
+++ alura/Composer/src/Buscador.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Alura\BuscardorCursos;
+
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+    /**
+     * @var Crawler
+     */
+    private $crawler;
+
+    /**
+     * Buscador constructor.
+     * @param ClientInterface $
+     */
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+ {
+
+     $this->httpClient = $httpClient;
+     $this->crawler = $crawler;
+
+     
+ }
+
+    public function buscar(string $url): array
+    {
+        $response = $this->httpClient->request(
+            'GET',
+            '$url'
+        );
+        $html = $response->getBody();
+        $this->crawler->addHtmlContent($html);
+        $elements = $this->crawler->filter('span.card-curso__nome');
+        $result =[];
+        foreach ($elements as $element){
+            $result[] = $element->textContent;
+        }
+        return $result;
+
+ }
+}
