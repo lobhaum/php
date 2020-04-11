c99dabd16cc02b7d0cb9c1f0894f8ac105a97a32
lobhaum <contato@eduardoluciano.com.br>
2020-04-11 18:06:23 -0300

    refatorado  de $buscador para $searcher

--- alura/Composer/buscar-cursos.php
+++ alura/Composer/buscar-cursos.php
@@ -1,9 +1,8 @@
 <?php
 
 require 'vendor/autoload.php';
-require 'src/Buscador.php';
 
-use Alura\BuscardorCursos\Buscador as Buscador;
+use Alura\BuscardorCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
@@ -12,8 +11,8 @@ $client = new Client([
     'base_uri' => 'https://www.alura.com.br/'
 ]);
 $crawler = new Crawler();
-$buscador = new Buscador($client, $crawler);
-$result = $buscador->buscar('/cursos-online-programacao/php');
+$searcher = new Buscador($client, $crawler);
+$result = $searcher->buscar('cursos-online-programacao/php');
 
-foreach ($result as $curso) echo $curso.PHP_EOL;
+foreach ($result as $course) echo $course . PHP_EOL;
 
