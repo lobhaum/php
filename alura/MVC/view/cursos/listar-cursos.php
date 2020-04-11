bec8cc81311fd19d550c4b3fc222e214dc3f391e
lobhaum <contato@eduardoluciano.com.br>
2020-04-11 06:44:47 -0300

    add file

--- alura/MVC/view/cursos/listar-cursos.php
+++ alura/MVC/view/cursos/listar-cursos.php
@@ -2,8 +2,9 @@
     <a href="novo-curso" class="btn btn-primary mb-2">Novo curso</a>
     <ul class="list-group">
         <?php foreach ($cursos as $curso) {?>
-            <li class="list-group-item">
+            <li class="list-group-item d-flex justify-content-between">
                 <?php echo $curso->getDescricao(); ?>
+                <a href="excluir-curso?id=<?= $curso->getId();?>" class="btn btn-danger btn-sm">Excluir</a>
             </li>
         <?php }?>
     </ul>
