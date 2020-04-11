0e6429414b986770fd986a1483907ee397b54b50
lobhaum <contato@eduardoluciano.com.br>
2020-04-11 18:34:50 -0300

    add config files

--- alura/Composer/src/Buscador.php
+++ alura/Composer/src/Buscador.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Alura\BuscardorCursos;
-
 
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DomCrawler\Crawler;
@@ -23,25 +21,21 @@ class Buscador
      * @param ClientInterface $
      */
     public function __construct(ClientInterface $httpClient, Crawler $crawler)
- {
-
-     $this->httpClient = $httpClient;
-     $this->crawler = $crawler;
-
-     
- }
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
 
     public function buscar(string $url): array
     {
-        $response = $this->httpClient->request('GET',$url);
+        $response = $this->httpClient->request('GET', $url);
         $html = $response->getBody();
         $this->crawler->addHtmlContent($html);
         $elements = $this->crawler->filter('span.card-curso__nome');
-        $result =[];
-        foreach ($elements as $element){
+        $result = [];
+        foreach ($elements as $element) {
             $result[] = $element->textContent;
         }
         return $result;
-
- }
+    }
 }
